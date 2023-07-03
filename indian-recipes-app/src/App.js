666574614ab143972a15b502ca280cf9576f68be
Aruna  Joshi <arunajoshi1013@gmail.com>
2023-07-02 18:39:52 -0700

    Update the App component to include client side routing

--- indian-recipes-app/src/App.js
+++ indian-recipes-app/src/App.js
@@ -2,16 +2,19 @@
 import './App.css';
 import RecipeList from './RecipeList';
 import RecipeDetails from './RecipeDetails';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-        <p>
-          Let's go.
-        </p>
-        <RecipeDetails />
-        <RecipeList />
-    </div>
+    <Router>
+      <div className="App">
+          <p>
+            Let's go.
+          </p>
+          <RecipeDetails />
+          <RecipeList />
+      </div>
+    </Router>
   );
 }
 
