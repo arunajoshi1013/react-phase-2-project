656368e580346445b3bd100aa76c8a0b7c1616d7
Aruna  Joshi <arunajoshi1013@gmail.com>
2023-07-02 18:38:50 -0700

    RecipeList: Update the RecipeList component to list all the recipes

--- indian-recipes-app/src/RecipeList.js
+++ indian-recipes-app/src/RecipeList.js
@@ -1,8 +1,30 @@
+import { useEffect, useState } from 'react'
+import Recipe from './Recipe';
 
 function RecipeList() {
+
+    const [ thisRecipe, setRecipe ] = useState([]);
+
+    useEffect(() => {
+        getAllRecipes();
+    }, []); 
+
+    const getAllRecipes = async () => {
+        const db_fetch = await fetch(`http://localhost:3002/recipes`);
+        const recipes = await db_fetch.json();
+        setRecipe(recipes);
+    }
+
   return (
-    <div>List of recipes</div>
-  )
+    <div>
+        {thisRecipe.map(my_recip => {
+            return (
+                <Recipe key={my_recip.id} recipe={my_recip} />
+            );
+        })}
+    </div>
+  );
+
 }
 
 export default RecipeList
